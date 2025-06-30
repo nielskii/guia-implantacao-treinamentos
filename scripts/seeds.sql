6d8eacd2ecfc890e16f0641f599ac3dce730a4e9
Daniel Borges <oindiobrg@gmail.com>
2025-06-30 16:41:22 -0300

    Create seeds.sql

---
+++ scripts/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO cursos (titulo, descricao) VALUES
+('Boas Práticas de Segurança da Informação', 'Treinamento obrigatório para todos os colaboradores.'),
+('Integração de Novos Funcionários', 'Conteúdo introdutório para novos membros da equipe.');
